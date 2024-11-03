6a0fad36a3eaeee67cb50c0b91bc432671a67cd8
Murat Ozcan <muratkerem@gmail.com>
2024-11-03 08:55:26 -0600

    18-feat: app routes main

--- src/components/validation-error-display.cy.tsx
+++ src/components/validation-error-display.cy.tsx
@@ -2,11 +2,6 @@ import {ZodError} from 'zod'
 import ValidationErrorDisplay from './validation-error-display'
 
 describe('<ValidationErrorDisplay />', () => {
-  it('should not render when there is no validation error', () => {
-    cy.wrappedMount(<ValidationErrorDisplay validationError={null} />)
-    cy.getByCy('validation-error').should('not.exist')
-  })
-
   it('should render validation errors correctly', () => {
     const mockError = new ZodError([
       {
@@ -30,5 +25,10 @@ describe('<ValidationErrorDisplay />', () => {
     cy.getByCy('validation-error').should('have.length', 2)
     cy.contains('Name is required')
     cy.contains('Year must be a number')
+  })
+
+  it('should not render when there is no validation error', () => {
+    cy.wrappedMount(<ValidationErrorDisplay validationError={null} />)
+    cy.getByCy('validation-error').should('not.exist')
   })
 })
