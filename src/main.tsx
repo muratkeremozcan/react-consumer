ebd075c223cd6b56351e3de296a65f8f1ee52139
Murat Ozcan <muratkerem@gmail.com>
2024-11-03 07:31:31 -0600

    18-feat: app routes main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import './index.css'
 import App from './App'
 
 const port = import.meta.env.VITE_PORT
