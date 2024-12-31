19feefd9a22a677f96eb98138f3fbed1395149e6
muratkeremozcan <muratkerem@gmail.com>
2024-12-31 08:28:01 -0600

    38-test: pw tests part 1

---
+++ src/components/movie-list.pw.tsx
@@ -0,0 +1,51 @@
+import {test, expect} from '@playwright/experimental-ct-react'
+import MovieList from './movie-list'
+import {generateMovie} from '@support/factories'
+import sinon from 'sinon'
+
+test.describe('<MovieList>', () => {
+  const sandbox = sinon.createSandbox()
+  const onDelete = sandbox.stub()
+
+  test.afterEach(() => {
+    sandbox.restore()
+  })
+
+  test('should show nothing with no movies', async ({mount}) => {
+    const component = await mount(<MovieList movies={[]} onDelete={onDelete} />)
+
+    await expect(component.getByTestId('movie-list-comp')).not.toBeVisible()
+  })
+
+  test('should show error with error', async ({mount}) => {
+    const component = await mount(
+      <MovieList movies={{error: 'boom'}} onDelete={onDelete} />,
+    )
+
+    await expect(component.getByTestId('movie-list-comp')).not.toBeVisible()
+    await expect(component.getByTestId('error')).toBeVisible()
+  })
+
+  test('should verify the movie and delete', async ({mount}) => {
+    const movie1 = {id: 1, ...generateMovie()}
+    const movie2 = {id: 2, ...generateMovie()}
+
+    const component = await mount(
+      <MovieList movies={[movie1, movie2]} onDelete={onDelete} />,
+    )
+
+    await expect(component.getByTestId('movie-list-comp')).toBeVisible()
+
+    const movieItems = component.getByTestId('movie-item-comp').all()
+    const items = await movieItems
+    expect(items).toHaveLength(2)
+    // with PW you have to use for await of, since you have to await the expect
+    for (const item of items) {
+      await expect(item).toBeVisible()
+    }
+
+    await component.getByText('Delete').first().click()
+    expect(onDelete.calledOnce).toBe(true)
+    expect(onDelete.calledWith(1)).toBe(true)
+  })
+})
